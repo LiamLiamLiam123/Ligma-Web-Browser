b140d1a3fca93fe79fe5497cc8e2b727564f6995
LiamLiamLiam123 <163282026+LiamLiamLiam123@users.noreply.github.com>
2026-02-17 13:18:55 +1100

    Add files via upload

---
+++ Old-Ligma/Ligma-Browser.py
@@ -0,0 +1,12 @@
+make a settings icon and put it next to the 3 dot thing put everything that should be a setting in the settings button don't make it a drop downmenu make it a difffernt window but still everything should still work as normal also add more settings make this browser the most customizable thing ever
+Keep the looks the looks of this browser the same just add more fuctionality
+
+
+
+
+
+
+
+
+
+
